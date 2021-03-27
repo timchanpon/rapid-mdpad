04fe92155a1a106e72a799d86fd98a7a405a3854
timchanpon <62697984+timchanpon@users.noreply.github.com>
2021-03-27 21:34:22 +0900

    Implement realtime markdown feature
    Squashed commit of the following:
    
    commit 948dd017a633f25a6d9a8f83057469effc3e4203
    Author: timchanpon <62697984+timchanpon@users.noreply.github.com>
    Date:   Sat Mar 27 21:30:55 2021 +0900
    
        Improve UI, Implement prediction of serial list
    
    commit 37919d005acad79e46f81324eb29a776cb2a6e62
    Author: timchanpon <62697984+timchanpon@users.noreply.github.com>
    Date:   Sat Mar 27 20:23:49 2021 +0900
    
        Apply dark theme
    
    commit df19ba17f5937b471ba0e478aef0334daf4283e8
    Author: timchanpon <62697984+timchanpon@users.noreply.github.com>
    Date:   Sat Mar 27 20:20:33 2021 +0900
    
        Create primitive markdown editor
    
    commit 1e35294381936a6d436c8a96dcb33feceb08df7d
    Author: timchanpon <62697984+timchanpon@users.noreply.github.com>
    Date:   Sat Mar 27 19:52:49 2021 +0900
    
        Fix process is undefined error
    
        https://github.com/SimulatedGREG/electron-vue/issues/871#issuecomment-800338181
    
    commit 3a145445b230aa979158cc6338596ec9c2d2a66e
    Author: timchanpon <62697984+timchanpon@users.noreply.github.com>
    Date:   Sat Mar 27 19:39:23 2021 +0900
    
        Install marked

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,76 @@
 <template>
-  <div id="app">
-    <landing-page></landing-page>
-  </div>
+<div id="root">
+  <textarea
+    :value="text"
+    @input="updateText"
+  />
+  <div v-html="compiledMarkdown" />
+</div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+import marked from 'marked';
 
-  export default {
-    name: 'rapid-mdpad',
-    components: {
-      LandingPage
-    }
-  }
+export default {
+  name: 'rapid-mdpad',
+  data() {
+    return {
+      text: '# Hello!',
+    };
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.text);
+    },
+  },
+  methods: {
+    updateText(e) {
+      this.text = e.target.value;
+      this.handleEnter(e);
+    },
+    handleEnter(e) {
+      if (e.inputType !== 'insertLineBreak') return;
+
+      const splitText = e.target.value.split('\n');
+      const lenLine = splitText.length;
+      const prevLine = splitText[lenLine - 2];
+      const isOrderedList = prevLine.match(/^\d*\.\s.*/);
+      const isUnorderedList = prevLine.match(/^[\*|-]\s.*/);
+
+      let prefix = '';
+
+      if (isOrderedList) {
+        const num = prevLine.match(/^\d*/);
+        prefix = `${parseInt(num) + 1}.\v`;
+      } else if (isUnorderedList) {
+        prefix = prevLine.slice(0, 2);
+      }
+
+      this.text += prefix;
+    },
+  },
+};
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss" scoped>
+#root {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+
+  textarea {
+    width: 50%;
+    height: 100%;
+    padding: 10px;
+    font-size: 1.5rem;
+  }
+
+  div {
+    width: 50%;
+    height: 100%;
+    padding: 10px;
+    font-size: 1.5rem;
+    overflow-y: scroll;
+  }
+}
 </style>

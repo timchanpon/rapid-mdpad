df19ba17f5937b471ba0e478aef0334daf4283e8
timchanpon <62697984+timchanpon@users.noreply.github.com>
2021-03-27 20:20:33 +0900

    Create primitive markdown editor

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div id="app">
-    <landing-page></landing-page>
-  </div>
+<div id="root">
+  <textarea v-model="text" />
+  <p v-html="compiledMarkdown" />
+</div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+import marked from 'marked';
 
-  export default {
-    name: 'rapid-mdpad',
-    components: {
-      LandingPage
-    }
-  }
+export default {
+  name: 'rapid-mdpad',
+  data() {
+    return {
+      text: '# Hello!',
+    };
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.text, { sanitize: true });
+    },
+  },
+};
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss" scoped>
+#root {
+  display: grid;
+  grid-template-rows: 100vh;
+  grid-template-columns: 1fr 1fr;
+
+  p {
+    padding: 10px;
+  }
+}
 </style>
